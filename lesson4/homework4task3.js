bcc25ce2b3150789fc455695eb0fefa36dcb876c
OlesiaPonomarenko <160940182+OlesiaPonomarenko@users.noreply.github.com>
2024-10-01 05:49:43 +0300

    up

--- lesson4/homework4task3.js
+++ lesson4/homework4task3.js
@@ -1,16 +1,16 @@
 // 2) Перевірка на високосний рік.
 // ..., але якщо ділиться на 100, то не високосний, але якщо ділиться на 400, то точно високосний
 
-const nowYears = 2024;
-const leapYearNew = 0 <= nowYears <= 1900;
-const leapYearOld = (nowYears % 400) === 0 && (nowYears % 100) === 0 ;
-// 1901 & 2099 підходить старе правило
-// 1700 
+const nowYears = 1600;
+// // 1901 & 2099 підходить старе правило
+// // 1700
 
-if ((0 <= nowYears && nowYears <= 1900 ) === leapYearOld ){
+const oldYears = 0 <= nowYears && nowYears <= 1900;
+const newYears = 1901 <= nowYears && nowYears <= 2099;
+
+if ((0 <= nowYears && nowYears <= 1900) === ((nowYears % 400) === 0) ){
     console.log(`${nowYears}  високосний рік, так що рік буде цікавим.`);
 } else {
-     console.log(`${nowYears} не високосний рік, можете розслабитися.`);
+    console.log(`${nowYears} не високосний рік, можете розслабитися.`)
 }
-
 
